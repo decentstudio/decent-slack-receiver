d5f1de6f3ff4202fe6fccc06fd6171286ff02e35
Brandon Ringe <brandon.ringe@gmail.com>
2017-05-19 15:43:55 -0500

    Receiving slash commands - needs refactoring

--- server.js
+++ server.js
@@ -4,30 +4,30 @@
 // =============================================================================
 
 // call the packages we need
-var express = require('express');        // call express
-var app = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var http = require('http');
+const express = require('express');        // call express
+const app = express();                 // define our app using express
+const bodyParser = require('body-parser');
+const http = require('http');
+const commandValidator = require('./command/command-validator');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+const port = process.env.PORT || 8080;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+const router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
   res.json({ message: 'hooray! welcome to our api!' });
 });
 
-router.post('/', function (req, res) {
-  console.log(req.body);
-
+// Endpoint for events
+router.post('/', (req, res) => {
   // URL verification required for the Slack Events API
   if (req.body.type === "url_verification") {
     res.send(req.body.challenge);
@@ -41,6 +41,8 @@ router.post('/', function (req, res) {
     res.send("Access denied.");
   }
 
+  console.log(req.body);
+
   // We must immediately (within 2 seconds) send a 200 response
   // or else Slack will retry the message
   res.status(200);
@@ -48,32 +50,32 @@ router.post('/', function (req, res) {
 
   // Send the event on to the event receiver
 
+});
 
-  // Just testing webhooks
-  if (req.body.event.text.includes('eth')) {
-    return;
+// Endpoint for slash commands
+router.post('/command', (req, res) => {
+  // Verify that the request is coming from slack by making sure
+  // the token sent is the same as our app token
+  // Comment this out if testing without using Slack
+  if (req.body.token !== "NzZYknIbrEjXjfqusU3Htxn5") {
+    res.status(400);
+    res.send("Access denied.");
   }
 
-  const options = {
-    hostname: 'https://hooks.slack.com',
-    port: 80,
-    path: '/services/T57D8BH3R/B5DQWM7EV/bhi8dfsXFuWkx4FQKP1a4kuM',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  if (!isValidCommand(req.body)) {
+    res.send({
+      'response_type': 'ephemeral',
+      'text': 'Sorry, that is not a valid command'
+    });
+  }
 
-  const request = http.request(options, (result) => {
-    console.log(result);
+  // Respond immediately so the user knows command was received
+  res.status(200);
+  res.json({
+    'response_type': 'ephemeral',
+    'text': 'Command received! One moment...'
   });
-
-  request.on('error', (error) => {
-    console.error(`Error: ${error.message}`);
-  });
-
-  request.write(`{"text": "Hello from the slack receiver."}`);
-  request.end();
-
+  console.log(req.body);
 });
 
 // more routes for our API will happen here
