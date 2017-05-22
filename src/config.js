ebe3b7445e9f458ef8b06eaee699c9afab1f34cd
Cameron Barre <cbarre@bcvevolve.com>
2017-05-21 20:34:44 -0700

    Add RabbitMQ config vars.

--- src/config.js
+++ src/config.js
@@ -5,4 +5,9 @@ let get = function (varName) {
 }
 
 export const HTTP_PORT = get('HTTP_PORT'),
-             SLACK_VERIFICATION_TOKEN = get('SLACK_VERIFICATION_TOKEN');
+             SLACK_VERIFICATION_TOKEN = get('SLACK_VERIFICATION_TOKEN'),
+             RABBITMQ_HOST = get('RABBITMQ_HOST'),
+             RABBITMQ_PORT = get('RABBITMQ_PORT'),
+             RABBITMQ_USER = get('RABBITMQ_USER'),
+             RABBITMQ_PASS = get('RABBITMQ_PASS'),
+             RABBITMQ_VHOST = get('RABBITMQ_VHOST');
